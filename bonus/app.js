53da841ed3fa6ed7ca2c58be888c0942c5b8ae95
andreadau <dauriaandrea99@gmail.com>
2021-01-10 18:30:52 +0100

    Bonus !END!

--- bonus/app.js
+++ bonus/app.js
@@ -7,12 +7,32 @@ Il numero ottenuto appare al centro del quadrato
 let vm = new Vue({
     el: "#app",
     data: {
-
+      rows: 6,
+      cells: 6,
+      table: [],
     },
     methods: {
-
+        gridGen(){
+            for (let i = 0; i < this.rows; i++) {
+                this.table.push([]);
+                    for (let j = 0; j < this.cells; j++) {
+                        this.table[i].push({num:""});
+                    }
+            }
+        },
+        getRandomNum(r,c){
+                axios
+                .get("https://flynn.boolean.careers/exercises/api/random/int")
+                .then(response => {
+                    let randomNum = response.data.response;
+                    this.table[r][c].num = randomNum;
+                }).catch(error => {
+                    console.log("error in the API");
+                    console.log(error);
+                });        
+        }
     },
     mounted(){
-        
+        this.gridGen();
     }
 })
