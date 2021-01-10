fb428076f8d2f1e2cc5de8f4ee6478aaf4beafc3
Andrea <dauriaandrea99@gmail.com>
2021-01-10 15:36:40 +0100

    Email api !END!

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
+/*
+Istruzioni:
+Genera 10 mail tramite api e stampale in una lista
+Usate Vue e Axios.
+La richiesta AJAX per gli indirizzi email inviatela al seguente link:
+https://flynn.boolean.careers/exercises/api/random/mail
+La lista di email va stampata a schermo nella pagina html
+*/
+
 let vm = new Vue({
     el: "#app",
     data: {
-
+        userEmails:[],
     },
     methods: {
-
+        getEmail(){
+            for (let i = 0; i < 10; i++) {
+            axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then(response => {
+                let userEmail = response.data.response;
+                this.userEmails.push(userEmail);
+            }).catch(error => {
+                console.log("error in the API");
+                console.log(error);
+            });
+            }
+        }
+    },
+    mounted(){
+        this.getEmail();
     }
-})
+});
